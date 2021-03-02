997c323a4cdd9a70feedb1e7de11415ed8e292bb
David Vargas <dvargas92495@gmail.com>
2021-03-02 17:39:55 -0600

    Fix key condition

--- src/schedule-social.ts
+++ src/schedule-social.ts
@@ -1,5 +1,7 @@
 import subMinutes from "date-fns/subMinutes";
 import AWS from "aws-sdk";
+import startOfMinute from "date-fns/startOfMinute";
+import addSeconds from "date-fns/addSeconds";
 
 const credentials = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -8,20 +10,21 @@ const credentials = {
 const dynamo = new AWS.DynamoDB({ apiVersion: "2012-08-10", credentials });
 
 export const handler = async () => {
-  const now = new Date();
-  const lastMinute = subMinutes(now, 1);
+  const now = startOfMinute(new Date());
+  const toMinute = addSeconds(now, 30);
+  const lastMinute = subMinutes(toMinute, 1);
   console.log("Querying posts from", lastMinute, "to", now);
   const items = await dynamo
     .query({
       TableName: "RoamJSSocial",
       IndexName: "primary-index",
-      KeyConditionExpression: "#d >= :l AND #d <= :h",
+      KeyConditionExpression: "#d BETWEEN :l AND :h",
       ExpressionAttributeValues: {
         ":l": {
           S: lastMinute.toJSON(),
         },
         ":h": {
-          S: now.toJSON(),
+          S: toMinute.toJSON(),
         },
       },
       ExpressionAttributeNames: {
