e2e9091502b006da6b0a068aaf9a4f49aa6877ba
David Vargas <dvargas92495@gmail.com>
2021-03-01 23:14:26 -0600

    deploy lambda

--- src/schedule-social.ts
+++ src/schedule-social.ts
@@ -1,7 +1,30 @@
-import subMinutes from 'date-fns/subMinutes';
+import subMinutes from "date-fns/subMinutes";
+import AWS from "aws-sdk";
 
-export const handler = () => {
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+};
+const dynamo = new AWS.DynamoDB({ apiVersion: "2012-08-10", credentials });
+
+export const handler = async () => {
   const now = new Date();
   const lastMinute = subMinutes(now, 1);
   console.log("Querying posts from", lastMinute, "to", now);
+  const Items = await dynamo
+    .query({
+      TableName: "RoamJSSocial",
+      IndexName: "primary-index",
+      KeyConditionExpression: "date >= :l AND date <= :h",
+      ExpressionAttributeValues: {
+        ":l": {
+          S: lastMinute.toJSON(),
+        },
+        ":h": {
+          S: now.toJSON(),
+        },
+      },
+    })
+    .promise();
+  console.log("Found", Items.Count);
 };
